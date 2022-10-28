622d38e3c7c88301f011fd0ada5c8723966a1201
Raschid Llamas <62359390+rshdd@users.noreply.github.com>
2022-10-28 11:44:58 -0500

    Raytracer V0.9

---
+++ src/edu/up/isgc/raytracer/objects/Polygon.java
@@ -0,0 +1,90 @@
+/**
+ * [1968] - [2021] Centros Culturales de Mexico A.C / Universidad Panamericana
+ * All Rights Reserved.
+ */
+package edu.up.isgc.raytracer.objects;
+
+import edu.up.isgc.raytracer.Intersection;
+import edu.up.isgc.raytracer.Ray;
+import edu.up.isgc.raytracer.Vector3D;
+import edu.up.isgc.raytracer.tools.Barycentric;
+import edu.up.isgc.raytracer.tools.Material;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This class calculates all barycentric coordinates and triangles from a polygon.
+ */
+public class Polygon extends Object3D {
+
+    public List<Triangle> triangles;
+
+    public List<Triangle> getTriangles() {
+        return triangles;
+    }
+
+    /**
+     * @param position receives the position where the polygonal object will be instantiated.
+     * @param triangles receives an array containing all the polygon's triangles.
+     * @param color receives an RGB color that will be applied to the polygon.
+     * @param material receives the render properties.
+     */
+    public Polygon(Vector3D position, Triangle[] triangles, Color color, Material material){
+        super(position, color, material);
+        setTriangles(triangles);
+    }
+
+    public void setTriangles(Triangle[] triangles) {
+        Vector3D position = getPosition();
+        Set<Vector3D> uniqueVertices = new HashSet<Vector3D>();
+        for(Triangle triangle : triangles){
+            uniqueVertices.addAll(Arrays.asList(triangle.getVertices()));
+        }
+
+        for(Vector3D vertex : uniqueVertices){
+            vertex.setX(vertex.getX() + position.getX());
+            vertex.setY(vertex.getY() + position.getY());
+            vertex.setZ(vertex.getZ() + position.getZ());
+        }
+
+        this.triangles = Arrays.asList(triangles);
+    }
+
+    /**
+     * Tells if a ray crashes with the polygon and returns the intersection.
+     * @param ray
+     * return Intersection
+     */
+    @Override
+    public Intersection getIntersection(Ray ray) {
+        double distance = -1;
+        Vector3D normal = Vector3D.ZERO();
+        Vector3D position = Vector3D.ZERO();
+
+        for(Triangle triangle : getTriangles()){
+            Intersection intersection = triangle.getIntersection(ray);
+            double intersectionDistance = intersection.getDistance();
+            if(intersection != null && intersectionDistance > 0 && (intersectionDistance < distance ||distance < 0)){
+                distance = intersectionDistance;
+                position = Vector3D.add(ray.getOrigin(), Vector3D.scalarMultiplication(ray.getDirection(), distance));
+                //normal = triangle.getNormal();
+                normal = Vector3D.ZERO();
+                double[] uVw = Barycentric.CalculateBarycentricCoordinates(position, triangle);
+                Vector3D[] normals = triangle.getNormals();
+                for(int i = 0; i < uVw.length; i++) {
+                    normal = Vector3D.add(normal, Vector3D.scalarMultiplication(normals[i], uVw[i]));
+                }
+            }
+        }
+
+        if(distance == -1){
+            return null;
+        }
+
+        return new Intersection(position, distance, normal, this);
+    }
+}
