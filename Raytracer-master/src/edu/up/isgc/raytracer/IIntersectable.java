a1803c6d28d0b783c9352d0b1ee46a32611355a2
Raschid Llamas <62359390+rshdd@users.noreply.github.com>
2022-10-28 11:40:22 -0500

    Delete Raytracer-master directory

--- Raytracer-master/src/edu/up/isgc/raytracer/IIntersectable.java
+++
@@ -1,9 +0,0 @@
-/**
- * [1968] - [2021] Centros Culturales de Mexico A.C / Universidad Panamericana
- * All Rights Reserved.
- */
-package edu.up.isgc.raytracer;
-
-public interface IIntersectable {
-    public abstract Intersection getIntersection(Ray ray);
-}
